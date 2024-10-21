562816fb2ed1c1d17f4181418d6bc37768156ce2
BeZoBrAzIe1703 <Ft20031405@mail.ru>
2024-10-21 09:38:34 -0700

    Add files via upload

---
+++ modul_4.1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as lie
+from true_math import divide as right
+
+
+result1 = lie(69, 3)
+result2 = lie(3, 0)
+result3 = right(49, 7)
+result4 = right(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
